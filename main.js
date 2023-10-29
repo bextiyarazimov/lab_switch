effd5890f783b0d07551a23f7cc00d21d5241915
bextiyar <azimovbextiyar1989@gmail.com>
2023-10-29 17:44:27 +0400

    added

---
+++ main.js
@@ -0,0 +1,71 @@
+
+////--Tapsiriq № 1 İstifadəçidən bir ölkə adı daxil etməsini istəyin. Daxil edilən ölkə adını SWITCH istifadə edərək yoxlayıb geriyə həmin ölkənin paytaxtını qaytarın. --------/////
+
+//    let country =prompt("Please enter country");
+//    let capital;
+//    switch(country){
+//    case "Turkey":
+//    capital="Ankara";
+//    break;
+//    case "Russian":
+//     capital="Moskow";
+//     break;
+//     case "USA":
+//     capital="New-York";
+//     break;
+//     default:
+//      "Data is not defined";
+//      break;   
+//    }
+//    alert("capital:"+capital);
+
+
+
+  ////--Tapsiriq № 2  İstifadəçidən saniyə daxil etməsini istəyin. Daxil edilən saniyənin, neçə saniyə və neçə dəqiqə olduğunu hesablayıb geriyə qaytarın 
+  
+//   let second =parseInt(prompt("Enter a second"));
+//   let minute =Math.floor(second/60);
+//   let lastSecond=second % 60;
+//   alert(minute +"deqiqe"+ "" + lastSecond + "saniye");
+
+
+
+ ////--Tapsiriq № 3 İstifadəçidən bir rəqəm daxil etməsini istəyin. Daxil edilən rəqəmin faktorialını hesablayın və ekranda nəticəni göstərin.
+        /// IF ELSE /////
+       
+        // function getSum(number) {
+        //     if(number === 0 || number === 1){
+        //         return 1;
+        //     }else {
+        //         return number* getSum(number-1);
+        //     }
+        // }
+        // let a = parseInt(prompt("Enter number"));
+        // let faktorial = getSum(a);
+        // alert(faktorial);
+    
+
+
+            ///// FOR /////
+//             let a = parseInt(prompt("Enter number"));
+//             let faktorial = 1;
+
+//            for (let i = 1; i <= a; i++) {
+//            faktorial=faktorial *i;
+// }
+
+//        alert(faktorial)
+
+
+
+
+
+
+
+
+
+    
+   
+
+    
+
